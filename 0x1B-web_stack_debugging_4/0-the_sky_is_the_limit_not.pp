5e7ca741102baef90bb6c7b56e6a85d2c9b1ac8e
otis <vincentotiskisia@gmail.com>
2023-10-19 13:10:19 +0000

    two'

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,6 @@
 # Increases the amount of traffic an Nginx server can handle.
-
+   
+loraising
 # Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
